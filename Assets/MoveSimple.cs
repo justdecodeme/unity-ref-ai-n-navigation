328b82322604460ac0a42b9860a4fb20e7dd7035
Rakesh Kumar <justdecodeme@hotmail.com>
2022-02-14 09:20:51 +0530

    movesimple and movetogoal scripts and examples added

---
+++ Assets/MoveSimple.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MoveSimple : MonoBehaviour {
+
+    public Vector3 goal = new Vector3(5, 0, 4);
+    public float speed = 1f;
+
+    private void LateUpdate() {
+        Debug.DrawRay(transform.position, goal.normalized * 5f, Color.red);
+        transform.Translate(goal.normalized * speed * Time.deltaTime);
+    }
+}
---
+++ Assets/MoveToGoal.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class MoveToGoal : MonoBehaviour {
+
+    // Object speed
+    public float speed = 2.0f;
+    // Accuracy
+    public float accuracy = 0.01f;
+    // Object target
+    public Transform goal;
+
+    // Update is called once per frame
+    void LateUpdate() {
+
+        // Calculate the facing direction
+        transform.LookAt(goal.position);
+
+        // Find the the direction vector
+        Vector3 direction = goal.position - transform.position;
+
+        // Draw debug ray in the direction your object is facing
+        Debug.DrawRay(transform.position, direction, Color.red);
+
+        // Check how close you character is to the target
+        if (direction.magnitude > accuracy) {
+
+            // Move the Object
+            transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
