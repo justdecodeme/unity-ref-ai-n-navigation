483970e2fa4c50404827dbd0eea518a2e427ed34
Rakesh Kumar <justdecodeme@hotmail.com>
2022-02-15 10:25:01 +0530

    enemyfollowpalyer example added

---
+++ Assets/_11. EnemyFollowPlayer/EnemyFollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyFollowPlayer : MonoBehaviour
+{
+    NavMeshAgent m_Agent;
+    public Transform player;
+
+    void Start()
+    {
+        m_Agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        m_Agent.destination = player.position;
+    }
+}
